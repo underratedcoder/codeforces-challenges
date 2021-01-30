bd4fe8fcec2cb5f5122d261b798b359895326f48
akashpatel268 <akashpatel268@gmail.com>
2021-01-30 13:46:21 +0530

    Added function in my template for sieve and to find prime number

--- my-template/template.cpp
+++ my-template/template.cpp
@@ -14,6 +14,35 @@ lli my_floor(lli a, lli b)
     return a / b;
 }
 
+boolean isPrime(lli n)
+{
+    if (n == 1)
+        return false;
+    if (n == 2)
+        return true;
+    if (n % 2 == 0)
+        return false;
+    for (lli i = 3; i * i <= n; i += 2)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+
+bool primes[31001];
+void calPrimes(lli n)
+{
+    memset(primes, true, sizeof(pr));
+    primes[0] = false;
+    primes[1] = false;
+    for (lli j = 2; j * j <= n; j++)
+    {
+        if (primes[j] == true)
+        {
+            for (lli i = j * j; i <= n; i += j)
+                primes[i] = false;
+        }
+    }
+}
 // code start from here...
 
 int main()
