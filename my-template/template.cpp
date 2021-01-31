791ac909b5bf6ec408ed43283a745f164bcfb255
akashpatel268 <akashpatel268@gmail.com>
2021-01-31 12:14:25 +0530

    Added more methods to my cpp template

--- my-template/template.cpp
+++ my-template/template.cpp
@@ -3,18 +3,130 @@ using namespace std;
 
 #define li long int
 #define lli long long int
+#define ulli unsigned long long int
+lli mod = 1000000007;
 
+bool isPowerOfTwo(lli n)
+{
+    if (n == 0)
+        return 0;
+    while (n != 1)
+    {
+        if (n % 2 != 0)
+            return 0;
+        n = n / 2;
+    }
+    return 1;
+}
+
+bool isPalindrome(string s)
+{
+    lli l = 0;
+    lli h = s.size() - 1;
+    while (h > l)
+    {
+        if (s[l++] != s[h--])
+        {
+
+            return false;
+        }
+    }
+    return true;
+}
+
+//to find nth fibonacci number.
+//This code returns Fn and Fn+1 as a pair.
+pair<lli, lli> fib(lli n)
+{
+    if (n == 0)
+        return {0, 1};
+
+    auto p = fib(n >> 1);
+    lli c = p.first * (2 * p.second - p.first);
+    lli d = p.first * p.first + p.second * p.second;
+    if (n & 1)
+        return {d, c + d};
+    else
+        return {c, d};
+}
+
+lli fact(lli n)
+{
+    lli res = 1;
+    for (lli i = n; i >= 1; i--)
+        (res *= i) %= mod;
+    return (res % mod);
+}
+
+lli power(lli a, lli b, lli mod)
+{
+    lli ans = 1;
+
+    a = a % mod;
+
+    while (b)
+    {
+        if (b & 1)
+            ans = (ans * a) % mod;
+        b /= 2;
+        a = (a * a) % mod;
+    }
+    return ans;
+}
+
+lli inv(lli a)
+{
+    return power(a, MOD - 2) % MOD;
+}
+
+//THIS IS THE CODE TO FIND ALL PRIME NUMBERS IN THE RANGE "L" to "R";
+vector<lli> segmentedSieve(lli L, lli R)
+{
+    // generate all primes up to sqrt(R)
+    lli lim = sqrt(R);
+    vector<bool> mark(lim + 1, false);
+    vector<lli> primes;
+    for (lli i = 2; i <= lim; ++i)
+    {
+        if (!mark[i])
+        {
+            primes.emplace_back(i);
+            for (lli j = i * i; j <= lim; j += i)
+                mark[j] = true;
+        }
+    }
+
+    vector<bool> isPrime(R - L + 1, true);
+    for (lli i : primes)
+        for (lli j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    vector<long long> realprime;
+    for (lli i = 0; i <= R - L; i++)
+    {
+        if (isPrime[i])
+        {
+            realprime.emplace_back(i + L);
+        }
+    }
+    return realprime;
+}
+
+// Returns ceiling for a/b
 lli my_ceil(lli a, lli b)
 {
     return (a + b - 1) / b;
 }
 
+// Returns floor for a/b
 lli my_floor(lli a, lli b)
 {
     return a / b;
 }
 
-boolean isPrime(lli n)
+// Check if a number is prime
+bool isPrime(lli n)
 {
     if (n == 1)
         return false;
@@ -28,6 +140,7 @@ boolean isPrime(lli n)
     return true;
 }
 
+// Calculate prime upto N
 bool primes[31001];
 void calPrimes(lli n)
 {
@@ -43,7 +156,24 @@ void calPrimes(lli n)
         }
     }
 }
+
+// Calculate GCD of a and b
+lli gcd(lli a, lli b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+// Calculate LCM of a and b
+lli lcm(lli a, lli b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+// #######################
 // code start from here...
+// #######################
 
 int main()
 {
